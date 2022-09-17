3fb69e119f807317826604b92a9b067e0f098276
cotnals99 <su.chae@icloud.com>
2022-09-17 12:47:11 +0700

    Sidebar Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,44 +1,56 @@
-import React from 'react'
-import styled from 'styled-components'
-import youtubeLogo from '../img/logo.png'
+import React from "react";
+import styled from "styled-components";
+import youtubeLogo from "../img/logo.png";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Container = styled.div`
-flex:1;
-background-color: #202020;
-height: 100vh;
-color: white;
-font-size:14px;
-`
+  flex: 1;
+  background-color: #202020;
+  height: 100vh;
+  color: white;
+  font-family: "Poppins", Open sans;
+  font-size: 14px;
+`;
 
 const Wrapper = styled.div`
-padding: 18px 26px;
-`
+  padding: 18px 26px;
+`;
 
 const Logo = styled.div`
-display: flex;
-align-items: center;
-gap: 5px;
-font-weight: bold;
-margin-bottom: 25px;
-`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  // font-weight: bold;
+  margin-bottom: 25px;
+  cursor: pointer;
+`;
 
 const Img = styled.img`
-height: 25px;
-`
+  height: 25px;
+`;
 
-
+const Item = styled.div`
+display: flex;
+align-items: center;
+gap: 20px;
+cursor: pointer:
+`;
 
 const Menu = () => {
   return (
     <Container>
       <Wrapper>
         <Logo>
-          <Img src={youtubeLogo}/>
+          <Img src={youtubeLogo} />
           Doubleda
         </Logo>
+        <Item>
+          <HomeIcon />
+          Home
+        </Item>
       </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
