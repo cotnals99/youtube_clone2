1412d7ef33e3c8acc5ae22fa5508a40708f8d1e7
cotnals99 <su.chae@icloud.com>
2022-09-18 10:04:26 +0700

    ThemeProvider, Sticky Menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, {useState} from 'react'
+import styled, { ThemeProvider } from 'styled-components'
 
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
+
+import { darkTheme, lightTheme } from "./utils/Theme";
 
 const Container = styled.div`
 display: flex;
@@ -10,22 +12,54 @@ display: flex;
 
 const Main = styled.div`
 flex:7;
+background-color: red;
 `;
 
 const Wrapper = styled.div``;
 
 const App = () => {
+
+  const [darkMode, setDarkMode] = useState(true)
   return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
     <Container>
-      <Menu/>
+      <Menu darkMode = {darkMode} setDarkMode={setDarkMode}/>
       <Main> 
         <Navbar/>
         <Wrapper>
-          video cards
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
+          <h1>Test</h1>
         </Wrapper>
       </Main>
 
     </Container>
+    </ThemeProvider>
   )
 }
 
